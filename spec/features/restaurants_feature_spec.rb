1f96dc97aeea8875608f70eeccf4cba94f818bb9
tbscanlon <thomas.scanlon@outlook.com>
2017-07-27 14:35:36 +0100

    Create restaurant views and model

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -8,4 +8,16 @@ feature 'Restaurants' do
       expect(page).to have_link 'Add a restaurant'
     end
   end
+
+  context "Restaurants have been added" do
+    before do
+      Restaurant.create(name: "KFC")
+    end
+
+    scenario "display restaurants" do
+      visit '/restaurants'
+      expect(page).to have_content 'KFC'
+      expect(page).to_not have_content "No restaurants yet"
+    end
+  end
 end
